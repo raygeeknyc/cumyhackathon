770be54fa7f55b6d4a4c437b1e0446496cf041cb
Raymond Blum <rsygeeknyc@gmail.com>
2018-04-14 20:34:40 -0400

    Start of judging app data model classes defined

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "cunyhackathon-1522753669833"
+#virtualenv --python python2 env
+echo "source env/bin/activate"
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/raymond/Documents/workspace/cunyhackathon/cunyhackathon-81a7d8d39eae.json
